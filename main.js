4ad30c396bf753a9724d18cb3bc8472af6de2151
bexqth <nicollsprite@gmail.com>
2025-05-21 19:45:59 +0200

    buttons layout

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
+const path = require('path'); 
 const { app, BrowserWindow } = require('electron');
+
+require('electron-reload')([
+  path.join(__dirname, 'index.html'),
+  path.join(__dirname, 'styles.css'),
+  path.join(__dirname, 'script.js'),
+  // add other files/folders you want to watch here
+], {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+  ignored: /main\.js/,
+});
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 500,
+    resizable: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
   });
+  
+  win.setMenuBarVisibility(false);
+  win.removeMenu();
+
 
   win.loadFile('index.html');
 }
